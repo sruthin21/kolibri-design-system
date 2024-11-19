8836f07ddacfee5127d48d87b4d8108f9588e031
sruthin21 <125492105+sruthin21@users.noreply.github.com>
2024-11-19 19:38:24 +0530

    linting error

--- docs/pages/ktooltip.vue
+++ docs/pages/ktooltip.vue
@@ -1,10 +1,15 @@
 <template>
 
   <DocsPageTemplate apiDocs />
+    <h1>
+      hello
 
+    </h1>
+      <h2>hello2</h2>
+         <h3>hello3</h3>
 </template>
 
-
+ 
 <script>
 
   export default {};
